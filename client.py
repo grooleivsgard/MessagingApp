16f868f953cd7b44b7bd7595d747c864045a698f
oscarfIt <89549477+oscarfIt@users.noreply.github.com>
2022-03-07 21:00:46 +0200

    The server can now handle multiple clients connecting at once. Messages sent are returned to the sender.

--- client.py
+++ client.py
@@ -4,17 +4,18 @@ import threading
     
 def main():
     FORMAT = 'utf-8'
-    serverName = "192.168.56.1"
-    serverPort = 12345
+    serverName = "192.168.0.106"
+    registerPort = 12345
+    receivePort = 12346
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     name = input("Enter your name:\n")
-    clientSocket.sendto(name.encode(FORMAT), (serverName, serverPort))
+    clientSocket.sendto(name.encode(FORMAT), (serverName, registerPort))
     while True:
         message = input("Type a message to someone:\n")
         if message == "Quit":
             break
         else:
-            clientSocket.sendto(message.encode(FORMAT), (serverName, serverPort))
+            clientSocket.sendto(message.encode(FORMAT), (serverName, receivePort))
             newMsg = clientSocket.recv(1024)
             print(newMsg.decode(FORMAT))
     clientSocket.close()
--- server.py
+++ server.py
@@ -1,51 +1,74 @@
 #CSC3002F Assignment 1: Server
 import socket
 import threading
+import queue
 
 class Server(threading.Thread):
 
     #Initialises the server object
-    def __init__(self, host, port):
+    def __init__(self, host, port1, port2, port3):
         self.host = host
-        self.port = port
+        self.registerPort = port1
+        self.receivePort = port2
+        self.sendPort = port3
         self.FORMAT = 'utf-8'
         self.clients = [] #List of clients connected to the server as Client objects (see below)
+        self.dataQueue = queue.Queue() #FIFO queue of data sent my clients
+        self.recpQueue = queue.Queue() #FIFO queue of recipients where data should be sent
 
     #Opens a socket and binds it to the IP and port number contained in the server object
     def run(self):
-        regSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            regSocket.bind((self.host, self.port))
-        except socket.error as e:
-            print(str(e))
-        print(f"[SERVER ACTIVE ON IP {self.host}] The Server is ready to receive...")
-        regThread = threading.Thread(target=self.register, args=(regSocket,)) #Starts the thread that registers the clients on the server
+        print(f"[SERVER ACTIVE ON IP {self.host}] The Server is ready to receive...")    
+        regThread = threading.Thread(target=self.register) #Starts the thread that registers the clients on the server
         regThread.start()
-
-    #Registers the client's name and address (IP + port number)
-    def register(self, rsocket):
-        #while True: #We will need this line to make sure multiple clients can cannot but for now it breaks the code :(
-        data, addr = rsocket.recvfrom(2048) #Receive the client's name 
-        client = Client(addr[0], addr[1], data.decode(self.FORMAT)) #Creat a Client object from name and address
-        self.clients.append(client) #Place the client in the Server's array
-        print(client.name + " is connected.")
         recvThread = threading.Thread(target=self.receive) #Start the thread which receives messages
         recvThread.start()
+        sendThread = threading.Thread(target=self.send) #Starts the thread which sends messages to clients
+        sendThread.start()
 
-    def receive(self):
-        rsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #Registers the client's name and address (IP + port number)
+    def register(self):
+        regSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-            rsocket.bind((self.host, self.port))
+            regSocket.bind((self.host, self.registerPort))
+        except socket.error as e:
+            print(str(e))
+        while True: #Wait for clients to register
+            data, addr = regSocket.recvfrom(2048) #Receive the client's name 
+            client = Client(addr[0], addr[1], data.decode(self.FORMAT)) #Creat a Client object from name and address
+            self.clients.append(client) #Place the client in the Server's array
+            print(client.name + " is connected.")
+
+    #Receives a message from a client
+    def receive(self):
+        recSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            recSocket.bind((self.host, self.receivePort))
         except socket.error as e:
             print(str(e))
         clientsOnline = ""
         for i in range(len(self.clients)):
             clientsOnline = clientsOnline + '\n' + self.clients[i].name #Creates a string displaying all the clients connected to the user. For use later
         while True:
-            data, addr = rsocket.recvfrom(1024) #Receive message
+            data, addr = recSocket.recvfrom(2048) #Receive message
+            self.dataQueue.put(data) #Places data on queue
+            self.recpQueue.put(addr) #Places repicient on queue
+            print(data.decode(self.FORMAT))
+
+    #Sends a message to a client (For now just returns to the client who sent it)
+    def send(self):
+        sendsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            sendsocket.bind((self.host, self.sendPort))
+        except socket.error as e:
+            print(str(e))            
+        while True:
+            data = self.dataQueue.get() #Retrieve the data
+            addr = self.recpQueue.get() #Retrieve the recipient
             data = data.decode(self.FORMAT)
-            data = f"You said: {data}" #Placeholder reply to show code executed correctly
-            rsocket.sendto(data.encode(self.FORMAT), addr)
+            message = f"You said: {data}" #Placeholder reply to show code executed correctly
+            sendsocket.sendto(message.encode(self.FORMAT), addr)
+
 
 class Client:
 
@@ -55,5 +78,5 @@ class Client:
         self.FORMAT = 'utf-8'
         self.name = name
 
-server = Server("192.168.56.1", 12345)
+server = Server("192.168.0.106", 12345, 12346, 12347)
 server.run()
