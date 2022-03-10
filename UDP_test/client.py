7bbce24581e04029f19486271e8613b0d442cbe3
gro97 <gro.oleivsgard@gmail.com>
2022-03-10 09:43:07 +0200

    Organized directories

---
+++ UDP_test/client.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import select
+import socket
+import os
+import sys
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+import json
+
+
+def get_messages(sock, epoll, username, color):
+    while True:
+        poll_result = epoll.poll()
+        for event in poll_result:
+            if event[1] == select.EPOLLIN:
+                data, addr = sock.recvfrom( 1024, socket.MSG_DONTWAIT ) # buffer size is 1024 bytes
+
+                #  parse the json from the server
+                message = json.loads(data.decode())
+
+                if message['username'] == username:
+                    continue
+
+                msg_str = message['message']
+
+                #  if there is no user name, don't display it
+                if message['username']:
+                    msg_str = message['username'] + ": " + msg_str
+
+                #  print the message, with colors if allowed
+                if len(message['message']) > 0:
+                    if allow_color:
+                        print("\033[%sm%s\033[0m" % (message['color'], msg_str))
+                    else:
+                        print(msg_str)
+
+
+def get_input(sock, username, color):
+    while True:
+        message = { "username" : username, "message" : input().strip(), "color" : color  }
+        sock.sendto( json.dumps(message).encode(), (server_ip, int(server_port)) )
+
+
+if __name__ == '__main__':
+
+    server_ip = sys.argv[1].strip()
+    server_port = sys.argv[2].strip()
+    username = sys.argv[3].strip()
+
+    color = 0
+    allow_color = True
+    try:
+        color = sys.argv[4]
+        if color == "--nocolor":
+            allow_color = False
+            color = "0"
+    except:
+        pass
+
+    sock = socket.socket( socket.AF_INET, # Internet
+                          socket.SOCK_DGRAM ) # UDP
+
+    #  use epoll to wait for read events
+    epoll = select.epoll()
+    epoll.register(sock, select.EPOLLIN)
+
+    thread.start_new_thread(get_input, (sock, username, color))
+    thread.start_new_thread(get_messages, (sock, epoll, username, color))
+
+    #  upon "connecting", send /hello and /who to announce our arrival and get a list
+    #  of other people in the room
+    message = { "username" : username, "message" : "/hello", "color" : color  }
+    sock.sendto( json.dumps(message).encode(), (server_ip, int(server_port)) )
+    message = { "username" : username, "message" : "/who", "color" : color  }
+    sock.sendto( json.dumps(message).encode(), (server_ip, int(server_port)) )
+    try:
+        while 1:
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print("bye")
+        message = { "username" : username, "message" : "/goodbye", "color" : color  }
+        sock.sendto( json.dumps(message).encode(), (server_ip, int(server_port)) )
+        sys.exit(0)
---
+++ UDP_test/server.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+'''
+Copyright 2020 Tom Slankard
+server usage:
+python server.py [ip address] [port]
+'''
+
+import socket
+import time
+import sys
+import json
+import select
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+def send(sock, data, clients):
+    """
+    Sends the specified message to each client in the clients list.
+    """
+    for client in clients:
+        sock.sendto(data.encode(), client[1])
+
+
+def create_response(message_json, client, clients):
+    """
+    Interprets the message from the client and responds accordingly,
+    sending the message to other clients when appropriate.
+    """
+    #  the /hello message_json announces the new client
+    if( message_json['message'].startswith("/hello") ):
+        return {"username" : "server", \
+                "message" : message_json['username'] + " joined the chat", \
+                "color" : 35 }, \
+               clients
+
+    #  the /who message is only echoed back to the client who sent it
+    #  The response is a list of other people in the chat room
+    elif ( message_json['message'].startswith("/who") ):
+        return {"username" : "server", \
+                "message" : "people in room: " + ', '.join([y[0] for y in clients]), \
+                "color" : 35 }, \
+               [client]
+
+    #  The /goodbye message let's everyone know we're leaving
+    elif ( message_json['message'].startswith("/goodbye") ):
+        clients.remove(client)
+        return {"username" : "server", \
+                "message" : message_json['username'] + " left the chat", \
+                "color" : 35 }, \
+               clients
+
+    #  The /me message creates an "emote"
+    elif ( message_json['message'].startswith("/me") ):
+        newmsg = message_json['username'] + message_json['message'][3:]
+        return {"username" : None, "message" : newmsg, "color" : message_json['color'] }, \
+               clients
+
+    #  All other messages are simply re-sent back to all clients
+    return {"username" : message_json['username'], \
+            "message" : message_json['message'], \
+            "color" : message_json['color'] }, \
+           clients
+
+
+def handle_client_data(data, addr, clients):
+    """Handles a message read from a new or existing client."""
+    message_json = json.loads(data)
+    client = (message_json['username'], addr)
+    if client not in clients:
+        clients.append(client)
+
+    if data:
+        response, clients = create_response(message_json, client, clients)
+        send(sock, json.dumps(response), clients)
+
+
+def wait_for_message_and_respond(sock, epoll, clients):
+    """Waits for a message and processes it."""
+    poll_result = epoll.poll()
+    for event in poll_result:
+        if event[1] == select.EPOLLIN:
+            data, addr = sock.recvfrom( 1024, socket.MSG_DONTWAIT ) # buffer size is 1024 bytes
+            logger.debug(data)
+            logger.debug(clients)
+            handle_client_data(data, addr, clients)
+
+
+if __name__ == '__main__':
+    """
+    Creates a socket and epoll object for monitoring it,
+    then handles messages on the socket forever.
+    """
+    UDP_IP=sys.argv[1]
+    UDP_PORT=int(sys.argv[2])
+
+    #  create a datagram socket
+    sock = socket.socket( socket.AF_INET, # Internet
+                          socket.SOCK_DGRAM ) # UDP
+
+    #  bind the socket to a port, to allow people to send info to it
+    sock.bind( (UDP_IP,UDP_PORT) )
+
+    #  use epoll to wait for read events
+    epoll = select.epoll()
+    epoll.register(sock, select.EPOLLIN)
+
+    clients = []
+    while True:
+        wait_for_message_and_respond(sock, epoll, clients)
+
